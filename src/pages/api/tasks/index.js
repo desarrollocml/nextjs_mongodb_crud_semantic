17f33bf7ac793bf1e471d3f871e7b999335c7952
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-03 19:59:52 -0300

    agregando tareas a base de datos

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -3,16 +3,20 @@ import { dbConnect } from "utils/mongoose";
 dbConnect();
 
 export default async function handler(req, res) {
- // console.log(req.method);
- const {body,method} = req;
+  // console.log(req.method);
+  const { body, method } = req;
   switch (method) {
     case "GET":
       const tasks = await Task.find();
       return res.status(200).json(tasks);
     //console.log(tasks);
     case "POST":
-      console.log(body)
+      /* console.log(body)
       return res.json("creating a task");
+     */
+      const newTask = new Task(body);
+      const savedTask = await newTask.save();
+      return res.status(201).json(savedTask)
 
     default:
       return res.status(400).json({ msg: "this method is not supported" });
