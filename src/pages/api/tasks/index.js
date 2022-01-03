2631a014efe3c36470a904eefe984c3d4a7dbcea
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-03 18:43:06 -0300

    verificacion metodo get

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,11 +1,16 @@
-//import { dbConnect } from "../../../utils/mongoose";
-//import Task from "../../../models/Task";
-import { dbConnect } from "utils/mongoose";
 import Task from "models/Task";
+import { dbConnect } from "utils/mongoose";
 dbConnect();
 
 export default async function handler(req, res) {
-  const tasks = await Task.find();
-  console.log(tasks);
-  res.status(200).json("tasks");
+ // console.log(req.method);
+  switch (req.method) {
+    case "GET":
+      const tasks = await Task.find();
+      return res.status(200).json(tasks);
+    //console.log(tasks);
+
+    default:
+      return res.status(400).json({ msg: "this method is not supported" });
+  }
 }
