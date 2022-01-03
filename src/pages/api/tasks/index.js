a64ca7fe15a2425530863c37b8e639611114a395
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-03 19:41:36 -0300

    metodo post-crear tarea

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -4,11 +4,15 @@ dbConnect();
 
 export default async function handler(req, res) {
  // console.log(req.method);
-  switch (req.method) {
+ const {body,method} = req;
+  switch (method) {
     case "GET":
       const tasks = await Task.find();
       return res.status(200).json(tasks);
     //console.log(tasks);
+    case "POST":
+      console.log(body)
+      return res.json("creating a task");
 
     default:
       return res.status(400).json({ msg: "this method is not supported" });
