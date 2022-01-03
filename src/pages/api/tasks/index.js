d451873f37fbb6ca5bbdaaaf35d0fa7c226c20a0
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-03 18:22:03 -0300

    jscongig ruta origin raiz por src

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,5 +1,7 @@
-import { dbConnect } from "../../../utils/mongoose";
-import Task from "../../../models/Task";
+//import { dbConnect } from "../../../utils/mongoose";
+//import Task from "../../../models/Task";
+import { dbConnect } from "utils/mongoose";
+import Task from "models/Task";
 dbConnect();
 
 export default async function handler(req, res) {
