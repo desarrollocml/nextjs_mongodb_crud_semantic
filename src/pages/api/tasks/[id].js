18c78cbf576ec4e83fd81c952a1c2e8d3d78d2c6
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-03 21:50:20 -0300

    obtener tarea por id

---
+++ src/pages/api/tasks/[id].js
@@ -0,0 +1,28 @@
+import { dbConnect } from "utils/mongoose";
+import Task from "models/Task";
+
+dbConnect();
+
+export default async (req, res) => {
+  const {
+    method,
+    query: { id },
+    body,
+  } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const task = await Task.findById(id);
+        if (!task) return res.staus(404).json({ msg: "Task not found" });
+        return res.status(200).json(task);
+      } catch (error) {
+        return res.status(500).json({ msg: error.message });
+      }
+
+    case "PUT":
+    case "DELETE":
+    default:
+      return res.status(400).json({ msg: "this method is not supported" });
+  }
+};
