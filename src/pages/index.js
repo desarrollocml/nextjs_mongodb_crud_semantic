1fa859fc55e33ada19d5671d7e32ca7270c8b7da
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-04 15:53:44 -0300

    vista para cuando no hay tareas, agregada

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,33 @@
 //import styles from "../styles/Home.module.css";
 import { Button, Card, CardContent, CardHeader, Container, Grid } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 export default function Home({ tasks }) {
- // console.log(tasks);
+ const router = useRouter();
+
+ if(tasks.length === 0)
+  return(
+    <Grid
+      centered 
+      verticalAlign="middle" 
+      columns= "1"
+      style={{height:"80vh"}}
+    >
+      <Grid.Row>
+        <Grid.Column textAlign="center">
+          <h1>The are no taks yet</h1>
+          <img src="https://img.freepik.com/vector-gratis/ningun-concepto-ilustracion-datos_108061-573.jpg?size=338&ext=jpg" />
+          <div>
+          <Button>
+            Create Task
+          </Button>
+          </div>
+       </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
+
   return (
     <Container>
       <Card.Group itemsPerRow={4}>
@@ -10,7 +36,13 @@ export default function Home({ tasks }) {
             <Card key={task._id}>
               <Card.Content>
                 <Card.Header>{task.title}</Card.Header>
+                <p>{task.description}</p>
               </Card.Content>
+              <Card.Content extra>
+                <Button primary>View</Button>
+                <Button primary>Edit</Button>
+              </Card.Content>
+
             </Card>
           ))
        
