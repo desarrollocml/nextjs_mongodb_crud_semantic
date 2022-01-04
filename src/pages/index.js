cc79e80f5b962387fbe35b1234deeba35966a0a7
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-03 23:03:02 -0300

    peticion a api propia

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
-
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>hola c</h1>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/tasks");
+  const task = await res.json();
+
+  console.log(task);
+  return {
+    props: {},
+  };
 }
