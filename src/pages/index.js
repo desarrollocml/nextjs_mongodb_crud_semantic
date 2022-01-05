ae5174587ce8daaccd57d380b22236efcb0d1bcc
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-04 23:32:20 -0300

    vista detalle en formato json stringify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,61 @@
 //import styles from "../styles/Home.module.css";
-import { Button, Card, CardContent, CardHeader, Container, Grid } from "semantic-ui-react";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Grid,
+} from "semantic-ui-react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Home({ tasks }) {
- const router = useRouter();
+  const router = useRouter();
 
- if(tasks.length === 0)
-  return(
-    <Grid
-      centered 
-      verticalAlign="middle" 
-      columns= "1"
-      style={{height:"80vh"}}
-    >
-      <Grid.Row>
-        <Grid.Column textAlign="center">
-          <h1>The are no taks yet</h1>
-          <img src="https://img.freepik.com/vector-gratis/ningun-concepto-ilustracion-datos_108061-573.jpg?size=338&ext=jpg" />
-          <div>
-          <Button>
-            Create Task
-          </Button>
-          </div>
-       </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  )
+  if (tasks.length === 0)
+    return (
+      <Grid
+        centered
+        verticalAlign="middle"
+        columns="1"
+        style={{ height: "80vh" }}
+      >
+        <Grid.Row>
+          <Grid.Column textAlign="center">
+            <h1>The are no taks yet</h1>
+            <img src="https://img.freepik.com/vector-gratis/ningun-concepto-ilustracion-datos_108061-573.jpg?size=338&ext=jpg" />
+            <div>
+              <Button>Create Task</Button>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
 
   return (
-    <Container style={{padding:'20px'}}>
+    <Container style={{ padding: "20px" }}>
       <Card.Group itemsPerRow={4}>
-        {
-          tasks.map((task)=>(
-            <Card key={task._id}>
-              <Card.Content>
-                <Card.Header>{task.title}</Card.Header>
-                <p>{task.description}</p>
-              </Card.Content>
-              <Card.Content extra>
-                <Button primary>View</Button>
-                <Button primary>Edit</Button>
-              </Card.Content>
-
-            </Card>
-          ))
-       
-        }
-         </Card.Group>
+        {tasks.map((task) => (
+          <Card key={task._id}>
+            <Card.Content>
+              <Card.Header>{task.title}</Card.Header>
+              <p>{task.description}</p>
+            </Card.Content>
+            <Card.Content extra>
+              <Button primary onClick={() => router.push(`/tasks/${task._id}`)}>
+                View
+              </Button>
+              <Button
+                primary
+                onClick={() => router.push(`/tasks/${task._id}/edit`)}
+              >
+                Edit
+              </Button>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
     </Container>
   );
 }
--- src/pages/tasks/[id].js
+++ src/pages/tasks/[id].js
@@ -1,8 +1,28 @@
+import Error from "next/error";
 
-export default function TaskDetail() {
-    return (
-        <div>
-            task detail
-        </div>
-    )
+export default function TaskDetail({task, error}) {
+  if (error && error.statusCode)
+    return <Error statusCode = {error.statusCode} title={error.statusText} />
+    return <div>{JSON.stringify(task)}</div>
+}
+
+export async function getServerSideProps({ query: { id } }) {
+  const res = await fetch(`http://localhost:3000/api/tasks/${id}`);
+
+  if (res.status === 200) {
+    const task = await res.json();
+    return {
+      props: {
+        task,
+      },
+    };
+  }
+  return {
+      props:{
+          error:{
+              statusCode : res.status,
+              statusText: "Invalid Id"
+          }
+      }
+  }
 }
