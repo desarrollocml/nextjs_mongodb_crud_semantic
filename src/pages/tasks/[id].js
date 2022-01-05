7052b1f46e144282d4e0ee4ecf9e29f626ce8f5c
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-05 01:32:58 -0300

    funcion de borrado de tareas

--- src/pages/tasks/[id].js
+++ src/pages/tasks/[id].js
@@ -1,7 +1,32 @@
 import Error from "next/error";
-import { Button, Grid } from "semantic-ui-react";
+import { Button, Grid, Confirm } from "semantic-ui-react";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function TaskDetail({ task, error }) {
+  const [confirm, setConfirm] = useState(false);
+  const { query, push } = useRouter();
+
+  const open = () => setConfirm(true);
+  const close = () => setConfirm(false);
+
+  const deleteTask = async () => {
+    const { id } = query;
+    try {
+      await fetch(`http://localhost:3000/api/tasks/${id}`, {
+        method: "DELETE",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async () => {
+    await deleteTask();
+    push("/");
+    close();
+  };
+
   if (error && error.statusCode)
     return <Error statusCode={error.statusCode} title={error.statusText} />;
   return (
@@ -15,17 +40,20 @@ export default function TaskDetail({ task, error }) {
         <Grid.Column textAlign="center">
           <h1>{task.title}</h1>
           <p>{task.description}</p>
-          <Button
-            color="red"
-            onClick={() => {
-              console.log(task._id);
-            }}
-          >
+          <Button color="red" onClick={open}>
             Delete
           </Button>
           <div></div>
         </Grid.Column>
       </Grid.Row>
+
+      <Confirm
+        content={`Are you sure to delete the task ${task._id}`}
+        header="Please confirm"
+        onConfirm={handleDelete}
+        onCancel={close}
+        open={confirm}
+      />
     </Grid>
   );
 }
